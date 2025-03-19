4325202cd677b86a3f72df170a4d438d8c464ecb
scottirios <lucas.2023014216@alunocesul.com>
2025-03-18 22:46:40 -0300

    create post and put

--- server.js
+++ server.js
@@ -1,31 +1,51 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const products = [];
+let products = [];
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Server online!');
 })
 
 app.get('/products', (req, res) => {
-    const { id } = req.query;
-    res.json({id}).send();
+    res.json({ products: products }).send();
 });
+
 
 app.post('/products', (req, res) => {
-    res.status(201).json(products).send();
+    const { name, size } = req.body;
+    products.push({id: products.length + 1, name, size});
+
+    res.status(201).json().send();
 });
 
-app.put('/products', (req, res) => {
-    res.status(204).json(products).send();
+
+app.put('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const productIndex = products.findIndex(product => product.id == id);
+    if (productIndex == -1) {
+        return res.status(404).send();
+    }
+
+    products[productIndex] = { ...products[productIndex], name};
+
+    res.status(200).json(products).send();
+    
 });
 
-app.patch('/products', (req, res) => {
-    res.status(204).json(products).send();
+app.patch('/products/:id', (req, res) => {
+    return res.status(200).json().send();
 });
 
 app.delete('/products', (req, res) => {
-    res.status(204).json(products).send();
+    const { id } = req.body;
+    products = products.filter(product => product.id != id);
+
+    res.status(200).json().send();
 });
 
 app.listen(port, () => {
